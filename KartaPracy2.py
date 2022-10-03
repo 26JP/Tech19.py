9155decab440d46439aef288d05ee623e5d82ad3
26JP <114557880+26JP@users.noreply.github.com>
2022-10-03 12:15:21 +0000

    dodanie 2 zadan (kartapracy2)

---
+++ KartaPracy2.py
@@ -0,0 +1,16 @@
+# zad 1
+
+# a = int(input())
+# if a % 3 ==0:
+#     print("TAK")
+# else:
+#   print("NIE")
+
+# zad 2
+
+a = int(input())
+if a>=100 and a<1000 and a % 17 ==0:
+  print("TAK")
+else:
+  print("NIE")
+  
