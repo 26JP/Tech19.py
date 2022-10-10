a8e9dd6ca85822440e08c4029a28fdaa01ed0f18
26JP <114557880+26JP@users.noreply.github.com>
2022-10-10 12:15:28 +0000

    karta pracy 3

---
+++ KartaPracy3.py
@@ -0,0 +1,24 @@
+# p, q = int(input()), int(input())
+# if (1.3 p > q):
+#   print("TAK")
+# else:
+#   print("NIE")
+
+# print(list(range(10)))
+# print(list(range(5)))
+# print(list(range(2,10)))
+# print(list(range(12,10)))
+# print(list(range(12,10,-1)))
+
+#zad 1
+# n = int(input())
+# for i in range(n):
+#   print(i**3 + 3, end=" ")
+
+# for i in range(10,22): print(i, end=" ")
+
+# for i in range(15,32,2): print(i, end=" ")
+
+for i in range
+
+
