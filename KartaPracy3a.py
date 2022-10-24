830f456ea3bbff7a5e51980856b87a1c0138c943
26JP <114557880+26JP@users.noreply.github.com>
2022-10-24 12:16:11 +0000

    DODANIE ZADANIA 3

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -4,11 +4,25 @@
 #   print("*-|", end=" ")
 
 #zad 2
-# n= int(input())
+# n = int(input())
 # for i  in range(1, n+1):
 #   print("*"*i, end=" ")
 #   if i%2:
 #     print("||", end=" ")
 #   else:
 #     print("--", end=" ")
-  
+
+#zad 3
+# n = int(input())
+# for i in range(1, n+1):
+#   print("*", end="")
+#   if i%2 ==1:
+#     print("|"*i, end="")
+#   else:
+#     print("-"*i, end="")
+
+#PRE-Tabliczka Mnożenia
+for i in range(1,11):
+  for j in range(1,11):
+   print(i*j, end="\t")
+  print()
