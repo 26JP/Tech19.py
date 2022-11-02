f9af146524a3bcf7905da4f9d695eafbc05408b8
26JP <114557880+26JP@users.noreply.github.com>
2022-11-02 07:23:28 +0000

    dodanie pre

--- KartaPracy3a.py
+++ KartaPracy3a.py
@@ -39,17 +39,68 @@
 # **
 # *
 
+#     *
+#    **
+#   ***
+#  ****
+# *****
+
+# *****
+#  ****
+#   ***
+#    **
+#     *
 #PRE NA 2 PETLE
+# n = int(input())
+
+# for i in range(n):
+#   for j in range(i+1):
+#     print("*", end="")
+#   print()
+
+# print()
+# print()
+
+# for i in range(n):
+#     for j in range(n - i):
+#       print("*", end="")
+#     print()
+
+# print()
+# print()
+
+# for i in range(n):
+#     for j in range(n - i - 1):
+#         print(" ", end="")
+#     for k in range(n - i - 1,n):
+#         print("*", end="")
+#     print()
+
+# print()
+# print()
+
+# for i in range(n):
+#   for j in range(i):
+#       print(" ", end="")
+#   for k in range(i, n):
+#       print("*", end="")
+#   print()
+
 n = int(input())
-for i in range(n):
-  for j in range(i+1):
-    print("*", end="")
-  print()
-
-print()
-print()
 
 for i in range(n):
-  for j in range(n-i):
-    print("*", end="")
+  for j in range(n):
+    if i + j <= n - 1:
+      print("*", end="")
+    else:
+      print(" ", end="")
   print()
+
+for i in range(n):
+  for j in range(n):
+    if i >= n - j - 1:
+      print("*", end="")
+    else:
+      print(" ", end="")
+  print()
+
