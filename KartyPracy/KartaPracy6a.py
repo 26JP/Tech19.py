10c127fd49b284fbe492329c0da5a486ab5fd5fb
26JP <114557880+26JP@users.noreply.github.com>
2022-12-14 07:12:45 +0000

    dodanie zadan 8 i 9

--- KartyPracy/KartaPracy6a.py
+++ KartyPracy/KartaPracy6a.py
@@ -49,12 +49,43 @@
 #     print(f"Mamy parkę: {n}, {i}")
 
 # Zad 6
-a = int(input())
-b = int(input())
-a = x
-b = y
-while y > 0:
-  x, y = y, x % y
-c = a//x
-d = a//x
-  print(f"{a}/{b} = {c}/{d}")
+# a = int(input())
+# b = int(input())
+# a = x
+# b = y
+# while y > 0:
+#   x, y = y, x % y
+# c = a//x
+# d = a//x
+#   print(f"{a}/{b} = {c}/{d}")
+
+# Zad 7
+
+# Zad 8
+# T = []
+# for i in range(2,10000):
+#   suma1 = 0
+#   for j in range(1, i):
+#     if i % j ==0:
+#       suma1 += j
+#   T.append(suma1)
+#   suma2 = 0
+#   for k in range(1,suma1):
+#     if suma1 % k ==0:
+#       suma2 += k
+#   if i == suma2 and suma1 != suma2 and suma2 not in T:
+#     print(f"({suma1},{suma2})")
+
+#zad 9
+def czy_pierwsza(n):
+  for i in range(2, n):
+    if n % i ==0:
+        return False
+    return True
+
+for i in range(10,100):
+  for j in range(2,i):
+    if i % j == 0:
+      if czy_pierwsza(j) and czy_pierwsza(i // j):
+        print(i, end=" ")
+        break
